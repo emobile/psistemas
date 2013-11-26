ca0625181659bc2a5a1ee35d377dcc2cbeebbfbd
’Alejandro <alexjabf@hotmail.com>
2013-11-25 17:19:18 -0700

    Cambios

--- app/controllers/mobile_services_controller.rb
+++ app/controllers/mobile_services_controller.rb
@@ -1,4 +1,8 @@
 class MobileServicesController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+  before_filter :detect_platform
+  respond_to :json
+  layout false
   def index
   end
   def get_driver_info
@@ -6,4 +10,17 @@ class MobileServicesController < ApplicationController
     current_date = (I18n.l Time.now, :format => :long_no_time)
     render json: @driver.blank? ?  {'status' => t('messages.info.driver_not_found')} : [@driver, @driver.user, @driver.truck, @driver.company, @driver.branch, current_date]
   end
+  
+  def detect_platform
+    #    if request.env['HTTP_USER_AGENT'] == ""
+    #      access = true
+    #    else
+    #      access = false
+    #    end
+    #    unless access
+    #      flash[:alert] = t('no_access')
+    #      redirect_to root_path
+    #    end
+  end
+  
 end
